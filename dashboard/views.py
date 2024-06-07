dcd8c6fb63b0074d635d90683c3de960e80a3aa9
LauraGenari <lauragenari018@usp.br>
2024-06-06 21:04:15 -0300

    error handling

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from .models import Asset
 from .forms import AssetForm
 import yfinance as yf
+from yfinance import shared
 
 def asset_list(request):
     assets = Asset.objects.all()
@@ -21,14 +22,25 @@ def asset_create(request):
 
 def asset_update(request, pk):
     asset = get_object_or_404(Asset, pk=pk)
+    error_message=''
     if request.method == 'POST':
         form = AssetForm(request.POST, instance=asset)
         if form.is_valid():
-            form.save()
-            return redirect('asset_list')
+            try:
+                stock = yf.Ticker(form.cleaned_data['ticker'])
+                hist = stock.history(period="1d")
+                if hist.empty:
+                    raise ValueError("Ticker não encontrado")
+                else:
+                    form.save()
+                    return redirect('asset_list')
+            except Exception as e:
+                error_message = f"Erro: {str(e)}"
+        else:
+            error_message = "Erro ao enviar o formulário. Verifique os dados e tente novamente."
     else:
         form = AssetForm(instance=asset)
-    return render(request, 'asset_form.html', {'form': form})
+    return render(request, 'asset_form.html', {'form': form, "error_message": error_message})
 
 @require_POST
 def asset_delete(request, pk):
